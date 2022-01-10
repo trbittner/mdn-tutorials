d35b03b8b84b3f74a3a991e40680de77312e0544
Todd Bittner <toddbittner@gmail.com>
2022-01-09 18:03:29 -0600

    Finished componentizing Todo.

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -2,6 +2,15 @@ import React from "react"
 import Todo from "./components/Todo.js"
 
 function App(props) {
+  const taskList = props.tasks?.map(task =>
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  )
+  
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -47,9 +56,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-      <Todo name="Eat" completed={true} id="todo-0" />
-      <Todo name="Sleep" completed={false} id="todo-1" />
-      <Todo name="Repeat" completed={false} id="todo-2" />
+      {taskList}
       </ul>
     </div>
   );
