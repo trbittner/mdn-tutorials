6ddc45d41d59be290f11be2a1217040494b870fa
Todd Bittner <toddbittner@gmail.com>
2022-01-19 20:14:41 -0600

    Added state to the React app.

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,55 +1,60 @@
-import React from "react"
+import React, { useState }  from "react"
 import Todo from "./components/Todo.js"
+import Form from "./components/Form.js"
+import FilterButton from "./components/FilterButton.js"
+import { nanoid } from "nanoid"
 
 function App(props) {
-  const taskList = props.tasks?.map(task =>
+  const [tasks,setTasks] = useState(props.tasks)
+  
+  function addTask(name) {
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false}
+    setTasks([...tasks, newTask])
+  }
+  
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      //If this task has the same ID as the edited task
+      if (id === task.id) {
+        //use Object spread to make a new object
+        //whose 'completed' prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task
+    })
+    setTasks(updatedTasks)
+  }
+  
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id)
+    setTasks(remainingTasks)
+  }
+
+  const taskList = tasks.map(task =>
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   )
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'
+  const headingText = `${taskList.length} ${tasksNoun} remaining`
   
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input label__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden"> Show </span>
-          <span> all </span>
-          <span className="visually-hidden"> tasks </span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden"> Show </span>
-          <span> Active </span>
-          <span className="visually-hidden"> tasks </span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden"> Show </span>
-          <span> Completed </span>
-          <span className="visually-hidden"> tasks </span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
