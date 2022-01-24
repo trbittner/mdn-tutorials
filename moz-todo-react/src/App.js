98c39cfa050e12337486c5c3268742c4b5ee814c
Todd Bittner <toddbittner@gmail.com>
2022-01-23 20:23:45 -0600

    Added final functionality to the todo app.

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -4,8 +4,17 @@ import Form from "./components/Form.js"
 import FilterButton from "./components/FilterButton.js"
 import { nanoid } from "nanoid"
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 function App(props) {
   const [tasks,setTasks] = useState(props.tasks)
+  const [filter,setFilter] = useState('All')
   
   function addTask(name) {
     const newTask = {id: "todo-" + nanoid(), name: name, completed: false}
@@ -29,8 +38,20 @@ function App(props) {
     const remainingTasks = tasks.filter(task => id !== task.id)
     setTasks(remainingTasks)
   }
+  
+  function editTask(id,newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, name:newName}
+      }
+      return task
+    })
+    setTasks(editedTaskList)
+  }
 
-  const taskList = tasks.map(task =>
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
     <Todo
       id={task.id}
       name={task.name}
@@ -38,8 +59,18 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
-  )
+  ))
+  
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ))
   
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'
   const headingText = `${taskList.length} ${tasksNoun} remaining`
@@ -49,9 +80,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
