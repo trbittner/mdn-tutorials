0059be00d1935648982c1d665c08e6b96f2e6bbf
Todd Bittner <toddbittner@gmail.com>
2022-01-27 19:22:42 -0600

    Created a first node web server and express app.

---
+++ express/hello.js
@@ -0,0 +1,19 @@
+//Load http module
+const http = require("http")
+
+const hostname = "127.0.0.1"
+const port = 8000
+
+//Create http server
+const server = http.createServer(function(req,res) {
+  //Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200,{'Content-Type': 'text/plain'})
+  
+  //Send the response body "Hello World"
+  res.end("Hello world\n")
+})
+
+server.listen(port,hostname,function() {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
+  
