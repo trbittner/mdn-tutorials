ba52d89651e1e858ef2bbdf0bf09b784260a59d7
Todd Bittner <toddbittner@gmail.com>
2022-02-10 19:49:53 -0600

    Added date formatting to the page.

--- express/express-locallibrary-tutorial/models/bookinstance.js
+++ express/express-locallibrary-tutorial/models/bookinstance.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon")
+
 var mongoose = require('mongoose')
 
 var Schema = mongoose.Schema
@@ -7,6 +9,12 @@ var BookInstanceSchema = new Schema ({
  imprint: {type: String, required: true},
  status: {type: String, required: true, enum: ['Available','Maintenance','Loaned','Reserved'],default: 'Maintenance'},
  due_back: {type:Date, default: Date.now},
+})
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED)
 })
 
 //Virtual for bookinstance's url
