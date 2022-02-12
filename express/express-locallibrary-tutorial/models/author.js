2575194b327495fce628019a143f24c2e0e67d59
Todd Bittner <toddbittner@gmail.com>
2022-02-12 13:31:37 -0600

    Created an author list view.

--- express/express-locallibrary-tutorial/models/author.js
+++ express/express-locallibrary-tutorial/models/author.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon")
+
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
@@ -28,27 +30,32 @@ AuthorSchema
   return fullname
 })
 
+//Virtual for author's birth
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function() {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ''
+})
+
+//Virtual for author's death
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function() {
+  return  this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ''
+})
+
 //Virtual for author's lifespan
 AuthorSchema
 .virtual('lifespan')
 .get(function() {
-  var lifetime_string = ''
-  if (this.date_of_birth) {
-    lifetime_string = this.date_of_birth.getYear().toString()
-  }
-  lifetime_string += ' - '
-  if (this.date_of_death) {
-    lifetime_string += this.date_of_death.getYear()
-  }
-  
-  return lifetime_string
+  return `${this.date_of_birth_formatted} - ${this.date_of_death_formatted}`
 })
 
 //Virtual for author's URL
 AuthorSchema
 .virtual('url')
 .get(function() {
-  return '/catalog/author' + this._id
+  return '/catalog/author/' + this._id
 })
 
 //Export model
