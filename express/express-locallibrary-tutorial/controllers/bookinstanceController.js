9917c6cc0cbabe42baf53ca5727cea719b93ef5b
Todd Bittner <toddbittner@gmail.com>
2022-02-20 17:08:35 -0600

    Added book instance details page.

--- express/express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express/express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -1,7 +1,7 @@
 var BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
-exports.bookinstance_list = function(req, res,next) {
+exports.bookinstance_list = function(req, res, next) {
   BookInstance.find()
   .populate('book')
   .exec(function (err,list_bookinstances) {
@@ -12,8 +12,18 @@ exports.bookinstance_list = function(req, res,next) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+  BookInstance.findById(req.params.id)
+  .populate('book')
+  .exec(function (err,bookinstance) {
+    if (err) {return next(err)}
+    if(bookinstance===null) {
+      var err = new Error('Book copy not found')
+      err.status = 404
+      return next(err)
+    }
+    res.render('bookinstance_detail',{title:`Copy ${bookinstance.book.title}`,bookinstance: bookinstance})
+  })
 };
 
 // Display BookInstance create form on GET.
