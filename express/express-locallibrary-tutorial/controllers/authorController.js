5a0bf6c7d69fd8e29c1de1e85b437ea7d49294af
Todd Bittner <toddbittner@gmail.com>
2022-02-20 16:43:17 -0600

    Added author details page.

--- express/express-locallibrary-tutorial/controllers/authorController.js
+++ express/express-locallibrary-tutorial/controllers/authorController.js
@@ -1,4 +1,6 @@
-var Author = require ('../models/author')
+var async = require('async')
+var Author = require('../models/author')
+var Book = require('../models/book')
 
 // Display list of all Authors.
 exports.author_list = function(req,res) {
@@ -12,8 +14,25 @@ exports.author_list = function(req,res) {
 }
 
 // Display detail page for a specific Author.
-exports.author_detail = function(req,res) {
-  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id)
+exports.author_detail = function(req,res,next) {
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.params.id)
+      .exec(callback)
+    },
+    authors_book: function(callback) {
+      Book.find({'author':req.params.id},'title summary')
+      .exec(callback)
+    },
+  }, function(err,results) {
+    if(err) {return next(err)}
+    if (results.author === null) {
+      var err = new Error('Author not found')
+      err.status = 404
+      return next(err)
+    }
+    res.render('author_detail',{title: 'Author Detail', author: results.author,author_books:results.authors_book})
+  })
 }
 
 // Display Author create form on GET.
